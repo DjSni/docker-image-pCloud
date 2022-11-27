36fbf764a707a3d32b0aea8f66a8755642c282f1
DjSni <31524614+DjSni@users.noreply.github.com>
2022-11-27 22:06:22 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,6 +4,7 @@ set -e
 # Options for starting Ganesha
 : ${PCLOUD_USER:=""}
 : ${PCLOUD_2FA:=""}
+: ${PCLOUD_CRYPT:=""}
 : ${PCLOUD_MOUNT:="/data"}
 : ${USER:="nobody"}
 : ${GROUP:="users"}
@@ -24,12 +25,17 @@ then
   PCLOUD_2FA=-t ${PCLOUD_2FA}
 fi
 
+if [ "${PCLOUD_CRYPT}" != "" ]
+then
+  PCLOUD_CRYPT=-c -y ${PCLOUD_CRYPT}
+fi
+
 if [ ! -f /root/.pcloud/data.db ]
 then
   echo "Starting pcloud Container, please run the folloring line to login to pcloud"
-  echo "/usr/bin/pcloudcc -u ${PCLOUD_USER} -m ${PCLOUD_MOUNT} -p -s ${PCLOUD_2FA}"
+  echo "/usr/bin/pcloudcc -u ${PCLOUD_USER} -m ${PCLOUD_MOUNT} -p -s ${PCLOUD_2FA} ${PCLOUD_CRYPT}"
   exec sleep infinity
 fi
 
 echo "Starting pCloud Daemon"
-exec /usr/bin/pcloudcc --username ${PCLOUD_USER} --mountpoint ${PCLOUD_MOUNT}
+exec /usr/bin/pcloudcc --username ${PCLOUD_USER} --mountpoint ${PCLOUD_MOUNT} -d
